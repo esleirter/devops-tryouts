a1ee1cd7e077d23f6c06c73417223b4ec94a725c
Alexander Vilchez <alexander.vilchez@gdpteam.com>
2022-11-26 23:02:05 -0500

    Adding pipeline and fixing node test

--- app/tests/string.test.js
+++ app/tests/string.test.js
@@ -1,10 +1,10 @@
 const { greating, bye } = require('../utils/string');
 
 test('Greating father', () => {
-    expect(greating('Father')).toBe('hi Father');
+    expect(greating('Father')).toBe('Hi Father');
 });
 
 
 test('Bye mother', () => {
-    expect(bye('mother')).toBe('bye bye mother');
+    expect(bye('mother')).toBe('Bye bye mother');
 });
